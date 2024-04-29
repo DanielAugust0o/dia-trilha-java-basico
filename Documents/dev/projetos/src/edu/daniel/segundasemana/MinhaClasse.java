9bc6567fd0567129460f642601328ba9a5341807
UnderClany <77458377+UnderClany@users.noreply.github.com>
2024-04-29 14:22:39 -0300

    Primeiro commit

---
+++ Documents/dev/projetos/src/edu/daniel/segundasemana/MinhaClasse.java
@@ -0,0 +1,26 @@
+package edu.daniel.segundasemana;
+
+public class MinhaClasse {
+	
+public static void main(String[] args) {
+	
+	String primeiroNome = "Daniel";
+	String segundoNome = "Augusto";
+	
+	String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+	
+	System.out.println(nomeCompleto);
+	
+	
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+	return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+	
+	
+	
+}
+
+
+
+}
